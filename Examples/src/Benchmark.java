64a07df925bbe8ace04149582b33622369bfe45f
Brian Craig <bcraig108@gmail.com>
2013-07-02 22:13:28 -0400

    SecretWord Updates with loops

---
+++ Examples/src/Benchmark.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dad
+ */
+public class Benchmark {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+        long endTime = startTime + 60000;
+        
+        long index = 0;
+        
+        while ( true ) {
+            double x = Math.sqrt(index);
+            
+            long now = System.currentTimeMillis();
+            
+            if ( now > endTime ) {
+                break;
+            }
+            index++;
+        }
+        System.out.println( index + " loops in one minute." );
+    }
+}
