2fcbc187bf4fa1d4616a242473f6782dc2ed357c
Brian Craig <bcraig108@gmail.com>
2013-07-22 21:32:36 -0400

    Resubmit Hour 4

---
+++ Examples/src/Hour4/BlankFiller.java
@@ -0,0 +1,10 @@
+package Hour4;
+
+class BlankFiller
+{
+    public static void main( String[] arguments )
+    {
+        System.out.println( "The " + arguments[0] + " " + arguments[1] + 
+                            " fox jumped over the " + arguments[2] + " dog." );
+    }
+}
