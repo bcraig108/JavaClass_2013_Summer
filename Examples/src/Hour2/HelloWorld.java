9b83ee1df3bee13f827a35e6b77a0fac18e4fdf2
Brian Craig <bcraig108@gmail.com>
2013-07-22 21:32:12 -0400

    Resubmit Hour 2

---
+++ Examples/src/Hour2/HelloWorld.java
@@ -0,0 +1,12 @@
+package Hour2;
+
+/**
+ * HelloWorld - Lesson 1
+ * @author Brian
+ */
+public class HelloWorld {
+    public static void main( String[] arguments ) {
+        String greeting = "Hello World!";
+        System.out.println( greeting );
+    }
+}
