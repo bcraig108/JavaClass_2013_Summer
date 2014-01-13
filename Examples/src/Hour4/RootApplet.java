13040fbf2bbf11fe54ac92cbdfd69b7372bc2186
Brian Craig <bcraig108@gmail.com>
2014-01-13 17:49:35 -0500

    Move number variable inside paint()

--- Examples/src/Hour4/RootApplet.java
+++ Examples/src/Hour4/RootApplet.java
@@ -4,15 +4,9 @@ import java.awt.Graphics;
 
 public class RootApplet extends javax.swing.JApplet
 {
-    int number;
-    
-    public void init()
-    {
-        number = 225;
-    }
-    
     public void paint(Graphics screen)
     {
+        int number = 225;
         screen.drawString( "The square root of " + number + 
                            " is " + Math.sqrt(number), 
                            5, 
