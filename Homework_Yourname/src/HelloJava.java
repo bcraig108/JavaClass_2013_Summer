b615adb350ad2998d07994900afdb0a1ffe8cf80
Brian Craig <bcraig108@gmail.com>
2014-02-06 21:21:22 -0500

    Homework examples

---
+++ Homework_Yourname/src/HelloJava.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class HelloJava{
+    public static void main(String[] arguments){
+        String yourname="Alec";
+        int myage=15;
+        System.out.println("Hello, my name is "+yourname+".");
+        System.out.println("I am "+myage+" years old.");
+        System.out.println("I love Java programming.");
+    }
+}
+
