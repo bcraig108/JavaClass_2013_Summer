0d39f583dd61444e71a09f7ba4e93cde90cd93c7
Brian Craig <bcraig108@gmail.com>
2014-02-06 21:23:05 -0500

    Clean Example

--- Homework_Yourname/src/HelloJava.java
+++ Homework_Yourname/src/HelloJava.java
@@ -5,11 +5,11 @@
 
 class HelloJava{
     public static void main(String[] arguments){
-        String yourname="Alec";
-        int myage=15;
-        System.out.println("Hello, my name is "+yourname+".");
-        System.out.println("I am "+myage+" years old.");
-        System.out.println("I love Java programming.");
+        String yourname = "Mr. Craig";
+        int myage = 38;
+        System.out.println( "Hello, my name is " + yourname + "." );
+        System.out.println( "I am " + myage + " years old." );
+        System.out.println( "I love Java programming." );
     }
 }
 
